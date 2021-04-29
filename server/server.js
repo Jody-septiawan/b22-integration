bf3a2fa10cd4e6a3242952ac60094c8902925baa
lenovo <yosepalexsander9@gmail.com>
2021-04-29 11:26:52 +0700

    Done socket backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const routers = require('./src/routers');
-const cors = require('cors');
-
 const app = express();
+const http = require('http');
+const routers = require('./src/routers');
+const {Server} = require('socket.io');
+const cors = require('cors');
+const server = http.createServer(app);
+const io = new Server(server);
+
+const { Chat } = require('./models');
 
 const port = 5000;
 
@@ -11,5 +16,47 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/v1', routers);
 app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static(__dirname + '/public'))
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+let interval;
+
+async function getChat() {
+  let chats = await Chat.findAll();
+  chats = JSON.parse(JSON.stringify(chats));
+  io.emit('all chat', chats);
+};
+
+io.on('connection', (socket) => {
+  console.log('a user connection: ', socket.id);
+  if (interval) {
+    clearInterval(interval)
+  }
+
+  interval = setInterval(async () => {
+    await getChat();
+  }, 5000);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnect');
+    clearInterval(interval);
+    socket.disconnect();
+  })
+
+  socket.on('chat message', async (data) => {
+    console.log('data received from client: ', data);
+    const chat = await Chat.create(data);
+    io.emit('chat message', chat.message);
+  })
+
+  socket.on('todos', () => {
+    
+    io.emit('donatur list', data)
+  })
+});
+
+
+server.listen(port, () => console.log(`Running on port ${port}`));
